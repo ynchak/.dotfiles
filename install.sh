4babcebd8855c4108fb1396a4f5b36902f9e8fa9
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-05-02 12:01:37 +0300

    update

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 # install nix
-curl -L https://nixos.org/nix/install | sh
+# curl -L https://nixos.org/nix/install | sh
 
 # source nix
-. ~/.nix-profile/etc/profile.d/nix.sh
+# . ~/.nix-profile/etc/profile.d/nix.sh
 
 # install packages
-nix-env -iA \
-	nixpkgs.zsh \
-	nixpkgs.stow \
-	nixpkgs.bat \
-	nixpkgs.gnumake \
-	nixpkgs.gcc \
-	nixpkgs.starship \
-	nixpkgs.volta \
-	nixpkgs.go \
+# nix-env -iA \
+#	nixpkgs.zsh \
+#	nixpkgs.stow \
+#	nixpkgs.bat \
+#	nixpkgs.gnumake \
+#	nixpkgs.gcc \
+#	nixpkgs.starship \
+#	nixpkgs.volta \
+#	nixpkgs.go \
+sudo pacman -Suy
+sudo pacman -S zsh stow bat make gcc starship go 
+curl https://get.volta.sh | bash
 # stow dotfiles
 stow git
 stow zsh
